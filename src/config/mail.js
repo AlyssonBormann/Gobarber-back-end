f34982cfbea6ddef5836b0b3c4548171c2ea46ab
alyssonbormann <alysson.bormann@gmail.com>
2020-02-03 00:57:12 -0300

    Queue created and email sent

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '08c4b77c3b58ed',
+    pass: '2dbfd56fa30f55',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
